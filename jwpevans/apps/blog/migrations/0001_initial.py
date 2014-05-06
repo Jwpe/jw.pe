500407712a60cd831bf28e18c7f8c6bc3cb2f53b
Jwpe <jwpevans@gmail.com>
2014-05-05 23:24:09 -0400

    Altered blog posts to be able to be written in Markdown (or HTML).

--- jwpevans/apps/blog/migrations/0001_initial.py
+++ jwpevans/apps/blog/migrations/0001_initial.py
@@ -10,57 +10,35 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         # Adding model 'Category'
         db.create_table('blog_categories', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('title', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50)),
         ))
-        db.send_create_signal('blog', ['Category'])
+        db.send_create_signal(u'blog', ['Category'])
 
         # Adding model 'Post'
         db.create_table('blog_posts', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('title', self.gf('django.db.models.fields.CharField')(max_length=200)),
             ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50)),
             ('author', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, blank=True)),
-            ('body', self.gf('tinymce.models.HTMLField')()),
+            ('body', self.gf('django.db.models.fields.TextField')()),
             ('tease', self.gf('django.db.models.fields.TextField')(blank=True)),
-            ('visits', self.gf('django.db.models.fields.IntegerField')(default=0)),
             ('status', self.gf('django.db.models.fields.IntegerField')(default=2)),
             ('allow_comments', self.gf('django.db.models.fields.BooleanField')(default=True)),
             ('publish', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
             ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
             ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
         ))
-        db.send_create_signal('blog', ['Post'])
+        db.send_create_signal(u'blog', ['Post'])
 
         # Adding M2M table for field categories on 'Post'
         db.create_table('blog_posts_categories', (
             ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('post', models.ForeignKey(orm['blog.post'], null=False)),
-            ('category', models.ForeignKey(orm['blog.category'], null=False))
+            ('post', models.ForeignKey(orm[u'blog.post'], null=False)),
+            ('category', models.ForeignKey(orm[u'blog.category'], null=False))
         ))
         db.create_unique('blog_posts_categories', ['post_id', 'category_id'])
-
-        # Adding model 'Settings'
-        db.create_table('blog_settings', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('site', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['sites.Site'], unique=True)),
-            ('site_name', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('author_name', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
-            ('copyright', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
-            ('about', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
-            ('twitter_url', self.gf('django.db.models.fields.URLField')(max_length=200, null=True, blank=True)),
-            ('rss_url', self.gf('django.db.models.fields.URLField')(max_length=200, null=True, blank=True)),
-            ('email_subscribe_url', self.gf('django.db.models.fields.URLField')(max_length=200, null=True, blank=True)),
-            ('page_size', self.gf('django.db.models.fields.PositiveIntegerField')(default=20)),
-            ('ping_google', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('disqus_shortname', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
-            ('meta_keywords', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
-            ('meta_description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
-            ('active_editor', self.gf('django.db.models.fields.IntegerField')(default=1)),
-            ('excerpt_length', self.gf('django.db.models.fields.IntegerField')(default=500)),
-        ))
-        db.send_create_signal('blog', ['Settings'])
 
     def backwards(self, orm):
         # Deleting model 'Category'
@@ -72,92 +50,63 @@ class Migration(SchemaMigration):
         # Removing M2M table for field categories on 'Post'
         db.delete_table('blog_posts_categories')
 
-        # Deleting model 'Settings'
-        db.delete_table('blog_settings')
-
     models = {
-        'auth.group': {
+        u'auth.group': {
             'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
         },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
             'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
-        'auth.user': {
+        u'auth.user': {
             'Meta': {'object_name': 'User'},
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         },
-        'blog.category': {
+        u'blog.category': {
             'Meta': {'ordering': "('title',)", 'object_name': 'Category', 'db_table': "'blog_categories'"},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        'blog.post': {
+        u'blog.post': {
             'Meta': {'ordering': "('-publish',)", 'object_name': 'Post', 'db_table': "'blog_posts'"},
             'allow_comments': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'}),
-            'body': ('tinymce.models.HTMLField', [], {}),
-            'categories': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['blog.Category']", 'symmetrical': 'False', 'blank': 'True'}),
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']", 'null': 'True', 'blank': 'True'}),
+            'body': ('django.db.models.fields.TextField', [], {}),
+            'categories': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['blog.Category']", 'symmetrical': 'False', 'blank': 'True'}),
             'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             'publish': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
             'status': ('django.db.models.fields.IntegerField', [], {'default': '2'}),
             'tease': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
-            'visits': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'})
         },
-        'blog.settings': {
-            'Meta': {'object_name': 'Settings'},
-            'about': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'active_editor': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
-            'author_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'copyright': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'disqus_shortname': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'email_subscribe_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
-            'excerpt_length': ('django.db.models.fields.IntegerField', [], {'default': '500'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'meta_description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'meta_keywords': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'page_size': ('django.db.models.fields.PositiveIntegerField', [], {'default': '20'}),
-            'ping_google': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'rss_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
-            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']", 'unique': 'True'}),
-            'site_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'twitter_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
-        },
-        'contenttypes.contenttype': {
+        u'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        },
-        'sites.site': {
-            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
-            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         }
     }
 
