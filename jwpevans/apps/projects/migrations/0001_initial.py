7c48e41afb95be58c607171b5cb14a3c78a23446
Jwpe <jwpevans@gmail.com>
2016-07-11 13:07:44 +0100

    Replaced south migrations with new-style django migrations

--- jwpevans/apps/projects/migrations/0001_initial.py
+++ jwpevans/apps/projects/migrations/0001_initial.py
@@ -1,34 +1,22 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Pun'
-        db.create_table(u'projects_pun', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('text', self.gf('django.db.models.fields.CharField')(max_length=1000)),
-            ('pun_target', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('url', self.gf('django.db.models.fields.URLField')(max_length=200)),
-        ))
-        db.send_create_signal(u'projects', ['Pun'])
+    dependencies = [
+    ]
 
-    def backwards(self, orm):
-        # Deleting model 'Pun'
-        db.delete_table(u'projects_pun')
-
-    models = {
-        u'projects.pun': {
-            'Meta': {'object_name': 'Pun'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'pun_target': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'text': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
-            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
-        }
-    }
-
-    complete_apps = ['projects']
+    operations = [
+        migrations.CreateModel(
+            name='Pun',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('text', models.CharField(max_length=1000)),
+                ('pun_target', models.CharField(max_length=100)),
+                ('url', models.URLField()),
+            ],
+        ),
+    ]
